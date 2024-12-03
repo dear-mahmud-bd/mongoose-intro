c98c187ca23fa648e74ebc4ff4fb32c341aa2cee
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2024-12-03 10:03:28 +0600

    basic mongoose

---
+++ src/app/modules/students/student.controller.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import { StudentServices } from "./student.service";
+
+
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+    const result = await StudentServices.createStudentIntoDB(studentData);
+
+    res.status(200).json({
+      success: true,
+      message: "student is created succesfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: "students are retrieved succesfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+
+    res.status(200).json({
+      success: true,
+      message: "single student data retrieved succesfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
